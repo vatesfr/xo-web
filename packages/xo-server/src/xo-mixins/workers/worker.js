f6d0815c0ef13dd570adb6434d0e19d8bb934d7d
Julien Fontanet <julien.fontanet@isonoe.net>
2020-01-21 15:59:04 +0100

    feat(xo-server/worker/mergeVhd): add debug logs

--- packages/xo-server/src/xo-mixins/workers/worker.js
+++ packages/xo-server/src/xo-mixins/workers/worker.js
@@ -1,6 +1,9 @@
 // @flow
 
 import defer from 'golike-defer'
+import fs from 'fs'
+import humanFormat from 'human-format'
+import ms from 'ms'
 import { type Remote, getHandler } from '@xen-orchestra/fs'
 import { mergeVhd as mergeVhd_ } from 'vhd-lib'
 
@@ -29,5 +32,47 @@ export const mergeVhd = defer(async function(
   await childHandler.sync()
   $defer.call(childHandler, 'forget')
 
-  return mergeVhd_(parentHandler, parentPath, childHandler, childPath)
+  const start = new Date()
+  const size = await mergeVhd_(
+    parentHandler,
+    parentPath,
+    childHandler,
+    childPath
+  )
+  const end = new Date()
+  const duration = end - start
+  fs.writeFile(
+    '/tmp/vhd-merge.log',
+    JSON.stringify(
+      {
+        child:
+          childHandler._getRealPath !== undefined
+            ? childHandler._getRealPath(childPath)
+            : {
+                remote: childRemote,
+                path: childPath,
+              },
+        parent:
+          parentHandler._getRealPath !== undefined
+            ? parentHandler._getRealPath(parentPath)
+            : {
+                remote: parentRemote,
+                path: parentPath,
+              },
+        start: start.toISOString(),
+        end: end.toISOString(),
+        duration: ms(duration),
+        size: humanFormat(size, { scale: 'binary', unit: 'B' }),
+        speed:
+          duration !== 0
+            ? humanFormat(size / duration, { scale: 'binary', unit: 'B/s' })
+            : 0,
+      },
+      null,
+      2
+    ) + '\n',
+    Function.prototype
+  )
+
+  return size
 })
