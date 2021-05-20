0b6d19407fcdc26d45e327d2de86315bde821aa2
Julien Fontanet <julien.fontanet@isonoe.net>
2021-05-20 22:41:59 +0200

    WiP: feat(self-signed): genSignedCert

--- @xen-orchestra/self-signed/index.js
+++ @xen-orchestra/self-signed/index.js
@@ -10,11 +10,30 @@ const openssl = (cmd, args, { input, ...opts } = {}) =>
     }
   })
 
-exports.genSelfSignedCert = async ({ days = 360 } = {}) => {
+const req = (key, selfSigned, { days = 360 } = {}) => {
+  const args = ['-batch', '-new', '-key', '-', '-days', String(days), '-nodes']
+  if (selfSigned) {
+    args.push('-x509')
+  }
+  return openssl('req', args, { input: key })
+}
+
+exports.genSelfSignedCert = async opts => {
   const key = await openssl('genrsa', ['2048'])
   return {
-    cert: await openssl('req', ['-batch', '-new', '-key', '-', '-x509', '-days', String(days), '-nodes'], {
-      input: key,
+    cert: await req(key, true, opts),
+    key,
+  }
+}
+
+exports.genSignedCert = async (ca, { days = 360 } = {}) => {
+  const key = await openssl('genrsa', ['2048'])
+  const csr = await req(key, false)
+  const input = [csr, key, ca.cert, ca.key].join('\n')
+  console.log(input)
+  return {
+    cert: await openssl('x509', ['-in', '-', '-signkey', '-', '-CA', '-', '-CAkey', '-', '-days', String(days)], {
+      input,
     }),
     key,
   }
